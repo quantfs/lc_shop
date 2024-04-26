e06fe89fe196b99f9f373f6d25789d85a72bc89f
Ivan <Zolotarev.iv@novomet.ru>
2024-04-26 13:33:07 +0500

    L8 Migration for product

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -146,7 +146,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="pages/gallery.html" class="nav-link">
+                        <a href="{{ route('product.index') }}" class="nav-link">
                             <i class="nav-icon fab fa-shopware"></i>
                             <p>
                                 Products
